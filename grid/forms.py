bc19849c545cb0deb4ab0c368b83b06c318617d6
Huong <hluu008@ucr.edu>
2019-10-18 13:15:41 -0700

    fix typo diff coeff

--- grid/forms.py
+++ grid/forms.py
@@ -11,7 +11,7 @@ class BasicForm(forms.Form):
 	nc = forms.IntegerField(required=False,label='Number of Columns:', min_value=2,max_value=20,widget=forms.TextInput(attrs={'placeholder': '8'}))
 	w = forms.FloatField(label='Channel Width:',help_text='mm',min_value=1e-10,max_value=1000,widget=forms.TextInput(attrs={'placeholder': '0.2'}))
 	l = forms.FloatField(label='Channel Length:',help_text='mm',min_value=1e-10,max_value=1000,widget=forms.TextInput(attrs={'placeholder': '1.5'}))
-	diffCoeff = forms.FloatField(label='Diffusion Coefficient:',help_text='mm^2/s',min_value=1e-10,max_value=10,widget=forms.TextInput(attrs={'placeholder': '4.25e-5'}))
+	diffCoeff = forms.FloatField(label='Diffusion Coefficient:',help_text='mm^2/s',min_value=1e-10,max_value=10,widget=forms.TextInput(attrs={'placeholder': '4.25e-4'}))
 	#initV = forms.FloatField(label='Inlet Velocity: ',help_text='mm/s')
 	input_type = forms.ChoiceField(choices = GRID_CHOICES, widget=forms.RadioSelect)
 	input_file = forms.FileField(required=False,label='Input Text File:')
